4b26b53511fed300a4d267596bf7e246e00c7023
chenglong.zou <chenglong.zou@estudiants.urv.cat>
2015-04-29 01:47:15 +0200

    Twitter twit API setup OK, todo twit carving up to 500MB put to mongoBD, after that datamining, TODO also -> 1r application design (view, controller & model), 2n data collection # hash source 3rd data processing procedure definition... 4th @report

--- routes/twitter.js
+++ routes/twitter.js
@@ -14,39 +14,82 @@ router.get('/', function (req, res, next) {
      res.status
      res.type -> Content-Type
      */
+    var queryTwitter = {
+        query: "#hashTag",
+        count: 10
+    }
 
-
-    //mongoose.connect(credentials.mongo.url, opts)
-
-    var queryResult;
-
-    Person.find({gender: 'F'}, function (err, persons) {
-        console.log(persons);
-        queryResult = {
-            persons: persons.map(function (p) {
-                return {
-                    name: p.name,
-                    hit: p.hit
-                }
-            })
-        };
-
-
-        // callback
-        console.log("CALLBACK");
-        var viewArgs = {
-            title: "Twitter",
-            content: queryResult
-        };
-        if (req.query.test !== undefined) {
-            viewArgs.test = req.query.test;
-            viewArgs.baseUrl = req.baseUrl.substring(1);
+    function syntaxHighlight(json) {
+        if (typeof json != 'string') {
+            json = JSON.stringify(json, undefined, 2);
         }
-        console.log(viewArgs);
-        res.render('twitter', viewArgs);
+        json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+        return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+            var cls = 'number';
+            if (/^"/.test(match)) {
+                if (/:$/.test(match)) {
+                    cls = 'key';
+                } else {
+                    cls = 'string';
+                }
+            } else if (/true|false/.test(match)) {
+                cls = 'boolean';
+            } else if (/null/.test(match)) {
+                cls = 'null';
+            }
+            return '<span class="' + cls + '">' + match + '</span>';
+        });
+    }
+
+    var twitter = require('../lib/twitter/twitter.js')({
+        consumerKey: credentials.twitter.ckey,
+        consumerSecret: credentials.twitter.csecret
+    });
+    // constructor del aplicatiu twitter
+
+    // query sobre algun hashtag
+    twitter.search(queryTwitter.query, queryTwitter.count, function (result) {
+        // tweets will be in results.statuses
+        queryTwitter.result = result.statuses;
+
+
+        var queryResult;
+
+
+        Person.find({gender: 'F'}, function (err, persons) {
+            console.log(persons);
+            queryResult = {
+                persons: persons.map(function (p) {
+                    return {
+                        name: p.name,
+                        hit: p.hit
+                    }
+                })
+            };
+
+
+            // callback
+            console.log("CALLBACK");
+            var viewArgs = {
+                title: "Twitter",
+                mongodb: queryResult,
+                twitter: queryTwitter
+            };
+            if (req.query.test !== undefined) {
+                viewArgs.test = req.query.test;
+                viewArgs.baseUrl = req.baseUrl.substring(1);
+            }
+            console.log(viewArgs);
+            res.render('twitter', viewArgs);
+
+
+        });
 
 
     });
+
+
+    //mongoose.connect(credentials.mongo.url, opts)
 
 
 });
